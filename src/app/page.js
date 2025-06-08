74224b009b06c8b939deab05f9eaacae7bfb0b34
Hadeslokiama <daradarcolinjames@gmail.com>
2025-06-08 22:36:04 +0800

    feat(page): implement FAQ section layout and integrate AccordionFAQ component

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,9 @@
-import Image from "next/image";
+import FAQ from "./faq";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
-          Password Generator
-        </h1>
-        <p className="text-center text-gray-600">
-          Coming soon...
-        </p>
-      </div>
+    <div className="min-h-screen bg-zinc-900">
+        <FAQ />
     </div>
   )
 }
